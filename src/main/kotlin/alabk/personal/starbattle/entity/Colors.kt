2343ce053ef981011cdf0d4fb9d983d21890a708
alabenkhlifa <ala.khliifa@gmail.com>
2024-07-30 15:26:24 +0100

    Added Colors enum

---
+++ src/main/kotlin/alabk/personal/starbattle/entity/Colors.kt
@@ -0,0 +1,6 @@
+package alabk.personal.starbattle.entity
+
+enum class Colors(val colorCode: String) {
+    Green("G"), Purple("Pu"), Orange("O"), Yellow("Y"),
+    Pink("PI"), Red("R"), Blue("B"), LightBlue("LB")
+}
